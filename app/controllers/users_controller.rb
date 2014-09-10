5f22686101d20846a01ea9a4d280e00ca9d8be6c
davidhurley <dhurls99@gmail.com>
2014-09-10 14:38:55 -0400

    change create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,15 +29,12 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to root_path, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    
+    if @user.save
+      user = login(params[:user][:email], params[:user][:password])
+      redirect_to root_path, notice: 'Thanks for joining!'
+    else
+      render :new
     end
   end
 
